bd74318a745e4e9b48c889909d3814b590e48aef
tvraghavan <tvraghavan@gmail.com>
2021-07-25 20:59:23 +0530

    added user api

--- src/main/java/com/example/controller/ExampleController.java
+++ src/main/java/com/example/controller/ExampleController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import com.example.model.User;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class ExampleController {
     @GetMapping("/version")
     public String version() {
         return "1.0";
+    }
+
+    @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getUser() {
+        return new User(1,"jsmith","ACTIVE");
     }
 }
